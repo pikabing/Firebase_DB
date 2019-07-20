f1a7cd9f8f8e5fcb3927791fb13cf6f0563ee2b8
Pratik Gupta <pgcool33@gmail.com>
2019-07-21 02:53:48 +0530

    add events done

--- app/src/main/java/android/example/zersey/AddEvent.java
+++ app/src/main/java/android/example/zersey/AddEvent.java
@@ -1,15 +1,122 @@
 package android.example.zersey;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
 public class AddEvent extends AppCompatActivity {
 
+    private MaterialButton image, create;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+    private DatabaseReference rootOfEvents = databaseReference.child("events");
+    private String title, desc, cat, imagePath;
+    private StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+    private StorageTask storageTask;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        TextInputEditText event_title, event_desc, event_cat;
+
+        event_title = findViewById(R.id.event_title);
+        event_desc = findViewById(R.id.event_desc);
+        event_cat = findViewById(R.id.event_category);
+        create = findViewById(R.id.create);
+        image = findViewById(R.id.event_image);
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int result = new UploadImage().requestPermission(AddEvent.this);
+                if (result == 2) {
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, 1);
+                }
+            }
+        });
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                title = event_title.getText().toString();
+                desc = event_desc.getText().toString();
+                cat = event_cat.getText().toString();
+
+                if (title.equals("") || desc.equals("")) {
+                    Toast.makeText(AddEvent.this, "Please give a name and a description.", Toast.LENGTH_SHORT).show();
+                } else if (storageTask != null && storageTask.isInProgress()) {
+                    Toast.makeText(AddEvent.this, "Upload in progress. Please wait.", Toast.LENGTH_SHORT).show();
+                } else {
+                    DatabaseReference childNode = rootOfEvents.child("title");
+                    childNode.setValue(title);
+                    childNode.child("desc").setValue(desc);
+                    childNode.child("category").setValue(cat);
+                    childNode.child("image").setValue(imagePath);
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK &&
+                data != null) {
+            Uri uri = data.getData();
+            new UploadImage();
+            String path = UploadImage.getPath(getApplicationContext(), uri);
+            if (path.equals("")) {
+                Toast.makeText(this, "Invalid. Please select from local storage only", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            imagePath = (uri.getPath().substring(uri.getPath().lastIndexOf("/") + 1));
+            image.setEnabled(false);
+            if (storageTask != null && storageTask.isInProgress()) {
+                Toast.makeText(this, "Uplaod in progress...", Toast.LENGTH_SHORT).show();
+            }else {
+                uploadImage(uri);
+            }
+        }
+    }
+
+    private void uploadImage(Uri uri) {
+
+        storageTask = storageReference.child(String.valueOf(System.currentTimeMillis())).putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(AddEvent.this, "Image Uploaded", Toast.LENGTH_SHORT).show();
+                image.setEnabled(true);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(AddEvent.this, "Upload Failed", Toast.LENGTH_SHORT).show();
+                image.setEnabled(true);
+            }
+        });
     }
 }
---
+++ app/src/main/java/android/example/zersey/UploadImage.java
@@ -0,0 +1,152 @@
+package android.example.zersey;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class UploadImage {
+
+    public static final int REQUEST_STORAGE_PERMISSION = 100;
+    Context mContext;
+
+    public int requestPermission(Context context) {
+
+        this.mContext = context;
+
+        if (PackageManager.PERMISSION_GRANTED !=
+                ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_STORAGE_PERMISSION);
+            } else {
+                Toast.makeText(context, "Permission has been denied", Toast.LENGTH_SHORT).show();
+            }
+        } else
+            return 2;
+        return 0;
+    }
+
+
+    public static String getPath(final Context context, final Uri uri) {
+
+        // check here to KITKAT or new version
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/"
+                            + split[1];
+                }
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"),
+                        Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{split[1]};
+
+                return getDataColumn(context, contentUri, selection,
+                        selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return "";
+    }
+
+    public static String getDataColumn(Context context, Uri uri,
+                                       String selection, String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {column};
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection,
+                    selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri
+                .getAuthority());
+    }
+
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri
+                .getAuthority());
+    }
+
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri
+                .getAuthority());
+    }
+
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri
+                .getAuthority());
+    }
+
+
+}
